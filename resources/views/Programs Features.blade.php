21d6a83c04dd31b93bc8def1b9e8c11a51a8c8c8
akhenz09 <108014956+akhenz09@users.noreply.github.com>
2022-07-14 21:28:21 +0800

    Major Changes

--- resources/views/Programs Features.blade.php
+++ resources/views/Programs Features.blade.php
@@ -1,5 +1,18 @@
 @extends('Layout')
 @section('content')
 
+<!-- Page Header Start -->
+<div class="container page-header">
+  <div class="container text-center pt-5 pb-3">
+      <h1 class="display-4 text-white">Programs Features</h1>
+          <ol class="breadcrumb justify-content-center mb-0">
+              <li class="breadcrumb-item"><a class="text-white" href="/">Home</a></li>
+              <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+              <li class="breadcrumb-item text-primary active" aria-current="page">About</li>
+          </ol>
+      </nav>
+  </div>
+</div>
+<!-- Page Header End -->
 
 @endsection
--- resources/views/Style.blade.php
+++ resources/views/Style.blade.php
@@ -7,8 +7,6 @@
 
 <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
@@ -25,12 +23,6 @@
 
 <!-- BS4 end -->
 
-
-
-<!-- AOS -->
-    <!--<link rel="stylesheet" href="aos-by-red.css">
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />-->
-<!-- AOS end -->
 
     </head>
 <style>
